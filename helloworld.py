f4cfec5f112a87de5b629c5f48298f407b6a3bd1
sana197111 <58847654+sana197111@users.noreply.github.com>
2021-08-20 17:52:41 +0900

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,23 +1,29 @@
-from re import S
-import mammoth
+import fnmatch
+import os
 import zipfile
-import os, sys
-from pdf2docx import Converter
-import fnmatch 
+
+import mammoth
 import openpyxl
 from bs4 import BeautifulSoup
+from pdf2docx import Converter
 import re
+import copy
 
 # ZIP 파일 압축 풀기
+extract_path = "/Users/gimsanha/개발/PythonWorkspace"
 
-extract_path = "/Users/gimsanha/개발/PythonWorkspace"
-zipfile.ZipFile('/Users/gimsanha/개발/PythonWorkspace/sample.zip').extractall(extract_path)
-zipfile_list = os.walk(extract_path+"/sample")
+zipfile.ZipFile('/Users/gimsanha/개발/PythonWorkspace/sample.zip').extractall(extract_path)
+
+zipfile_list = os.walk(extract_path + "/sample")
+
 
 # os.rmdir(extract_path +"/__macosx")
 
+
 # 파일 구분
+
 xlsx_pattern = "*.xlsx"
+
 pdf_pattern = "*.pdf"
 docx_pattern = "*.docx"
 html_pattern = "*.html"
@@ -25,111 +31,79 @@ standard_html_pattern = "standard.html"
 standard_excel_pattern = "standard.xlsx"
 result = []
 
+template_re = re.compile('([1-9][0-9]*)\?')
+template_path = dict()
+max_index = 0
+
+
+def dfs_children(obj, p):
+    global max_index
+    for i, c in enumerate(obj.children):
+        if c.name == 'p' and template_re.match(c.string):
+            print(template_re.findall(c.text)[0])
+            index = int(template_re.findall(c.text)[0])
+            template_path[index] = copy.deepcopy(p)
+            max_index = max(max_index, index)
+            print(template_path)
+        p.append(i)
+        dfs_children(c, p)
+        p.pop()
+
+def navigate(obj, p):
+    for i in p:
+        obj = obj.children[i]
+    return obj.text
+
+
 for root, dirs, files in zipfile_list:
     for name in files:
-            # docs 파일이라면 html 로 변경
-            if fnmatch.fnmatch(name, docx_pattern):
-                docx_path = os.path.join(root, name)
-                with open(docx_path, "rb") as docx_file:
-                    output = mammoth.convert_to_html(docx_file)
-                with open(docx_path.replace("docx","html"), "w") as html_file:
-                    html_file.write(output.value)
+        # docs 파일이라면 html 로 변경
+        if fnmatch.fnmatch(name, docx_pattern):
+            docx_path = os.path.join(root, name)
+            with open(docx_path, "rb") as docx_file:
+                output = mammoth.convert_to_html(docx_file)
+            with open(docx_path.replace("docx", "html"), "w") as html_file:
+                html_file.write(output.value)
 
-            # pdf 파일 -> docs 파일 -> html 파일로 변경
-            elif fnmatch.fnmatch(name, pdf_pattern):
-                pdf_path = os.path.join(root, name)
-                docx_path = pdf_path.replace("pdf","docx")
-                cv = Converter(pdf_path)
-                cv.convert(docx_path)
-                cv.close()
+        # pdf 파일 -> docs 파일 -> html 파일로 변경
+        elif fnmatch.fnmatch(name, pdf_pattern):
+            pdf_path = os.path.join(root, name)
+            docx_path = pdf_path.replace("pdf", "docx")
+            cv = Converter(pdf_path)
+            cv.convert(docx_path)
+            cv.close()
 
-                # docs 파일 -> html 파일
-                with open(docx_path, "rb") as docx_file:
-                    output = mammoth.convert_to_html(docx_file)
-                with open(docx_path.replace("docx","html"), "w") as html_file:
-                    html_file.write(output.value)
-
-
-            #excel 기준 파일 1? 2? 3? 4? 5? 6? 7? 데이터 위치 가져오기        
-            elif fnmatch.fnmatch(name, standard_html_pattern):
-                html_name = os.path.join(root,name)
-
-                html_name_html = open(html_name, "r")
-                soup = BeautifulSoup(html_name_html, "html.parser", from_encoding='utf=8')
-
-            else:
-                print("바꾸지 못하는 파일이 존재합니다")
+            # docs 파일 -> html 파일
+            with open(docx_path, "rb") as docx_file:
+                output = mammoth.convert_to_html(docx_file)
+            with open(docx_path.replace("docx", "html"), "w") as html_file:
+                html_file.write(output.value)
 
 
+        # excel 기준 파일 1? 2? 3? 4? 5? 6? 7? 데이터 위치 가져오기
+        elif fnmatch.fnmatch(name, standard_html_pattern):
+            html_name = os.path.join(root, name)
 
-        # re.match("<*>", objs_text)
-        # p = re.compile(r'^<\d+$>')
-        # mc = p.findall(objs_text)
-        # print(mc)
+            html_name_html = open(html_name, "r")
+            soup = BeautifulSoup(html_name_html, "html.parser", from_encoding='utf=8')
 
-# sibling 판단 후, 몇번째 자식인지 결정하는 함수
-def obj_next(objs):
-    if objs.find_next_siblings == None :
-        return
-    else:
-        objs_text = objs.find_next_siblings
-
-def obj_prev(objs):
-    if objs.find_previous_siblings == None :
-        return
-    else:
-        objs.find_previous_siblings
-
-
-# selector 뽑아내는 함수
-def obj_path(objs):
-    if objs.parent == None :
-        return
-
-    objs = objs.parent
-    obj_path(objs)
-
-    # print(objs)
-
-# 기준 파일에 필요한 노드의 selector 데이터 저장
-standard_list = ["1?","2?","3?","4?","5?","6?","7?","8?","9?","10?","11?","12?","13?","14?","15?","16?","17?","18?","19?","20?"]
-
-for obj in soup.find_all('p') : 
-    for i in standard_list:
-        if obj.text == i :
-            print(obj)
-            print(obj.parent)
-
-print(soup.find('p').name)
-
-            # obj.findParent
-            # print(obj_next(obj.parent))
-            # print(obj_prev(obj.parent))
-            # obj_next(obj)
-            # print(obj.parent.parent.find_previous_siblings)
-            # print(obj.parent.parent.find_next_siblings)
-            # print(obj.find_parent)
-            # obj_path(obj)
-            # obj_next(obj.parent.parent)
-
-# print(soup.find_all("table"))
-
-print(soup.select_one('table:nth-child(5) > tr:nth-child(2) > td:nth-child(1)'))
-# body > table:nth-child(5) > tbody > tr:nth-child(2) > td:nth-child(1) > p > strong
+            dfs_children(soup, [])
+        else:
+            print("바꾸지 못하는 파일이 존재합니다")
 
 # 날짜 추출
 for root, dirs, files in zipfile_list:
     for name in files:
         if fnmatch.fnmatch(name, html_pattern):
-            html_name = os.path.join(root,name)
+            html_name = os.path.join(root, name)
 
             html_name_html = open(html_name, "r")
             soup = BeautifulSoup(html_name_html, "html.parser", from_encoding='utf=8')
-            for obj in soup.find_all('p') : 
-                if obj.text == "\d\d\d\d. \d\d. \d\d":
-                    day = obj.text
-                    print(day)
 
+            for i in range(1, max_index + 1):
+                content = navigate(soup, template_path[i])
+                print(content)
+                # i-th content of html
 
 # html -> excel 파일 변경
 
@@ -139,5 +113,6 @@ for root, dirs, files in zipfile_list:
             excel_path = os.path.join(root, name)
             excelfile = openpyxl.load_workbook(excel_path)
             sheet = excelfile.active
-            sheet.append([day])
+            sheet.append(content)
             excelfile.save("standard_fin.xlsx")
+            print("마무리 되었습니다!")
